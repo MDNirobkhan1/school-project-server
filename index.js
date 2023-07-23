a4f28f195cc73fd6ebe95bfddf9a3643b631320a
MDNirobkhan1 <nirob937612@gmail.com>
2023-07-24 01:54:09 +0600

    last commit

--- index.js
+++ index.js
@@ -15,33 +15,48 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
 
 
-    const collegeCollection =client.db("college").collection("collegeBooking");
+        const collegeCollection = client.db("college").collection("collegeBooking");
+        const studentCollection = client.db("college").collection("student");
 
-    app.get('/collegeBooking', async (req,res)=>{
-        const result= await collegeCollection.find().toArray();
-        res.send(result)
-    })
+        app.get('/collegeBooking', async (req, res) => {
+            const result = await collegeCollection.find().toArray();
+            res.send(result)
+        })
 
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.post('/student', async (req, res) => {
+            const user = req.body;
+            console.log(user, 'user');
+            const result = await studentCollection.insertOne(user);
+            console.log('result', result);
+            res.send(result)
+        })
+
+        app.get('/student', async (req, res) => {
+            const result = await studentCollection.find().toArray();
+            console.log('result', result);
+            res.send(result)
+        })
+
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
